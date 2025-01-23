323cb965b0c9ff9511abc858c6718382d5a7ed07
AlekKostic <kostic.aleksandar006@gmail.com>
2025-01-23 17:27:55 +0100

    Nalazenje Usera po id

--- Backend/Backend/src/main/java/com/example/Backend/Controllers/UserController.java
+++ Backend/Backend/src/main/java/com/example/Backend/Controllers/UserController.java
@@ -1,13 +1,20 @@
 package com.example.Backend.Controllers;
 
+import com.example.Backend.DTO.PostDTO;
+import com.example.Backend.DTO.UserDTO;
+import com.example.Backend.DTO.UserLoginDTO;
+import com.example.Backend.Models.Like;
+import com.example.Backend.Models.Postovi;
 import com.example.Backend.Models.User;
 import com.example.Backend.Services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/api")
@@ -21,15 +28,20 @@ public class UserController {
         return ResponseEntity.ok(savedUser);
     }
 
-    @GetMapping(value = "/login/{username}/{password}")
-    public ResponseEntity<Optional<User>> findUser(@PathVariable("username") String  username, @PathVariable("password") String password){
+    @GetMapping(value = "/login")
+    public ResponseEntity<Optional<User>> findUser(@RequestBody UserLoginDTO userLoginDTO){
 
-        Optional<User> user = userServices.findUser(username,password);
+        Optional<User> user = userServices.findUser(userLoginDTO);
         if(user.isPresent()){
             return ResponseEntity.ok(user);
         }else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO find(@PathVariable("id") Long id){
+        return userServices.find(id);
     }
 
 }
--- Backend/Backend/src/main/java/com/example/Backend/Services/UserServices.java
+++ Backend/Backend/src/main/java/com/example/Backend/Services/UserServices.java
@@ -1,11 +1,16 @@
 package com.example.Backend.Services;
 
+import com.example.Backend.DTO.UserDTO;
+import com.example.Backend.DTO.UserLoginDTO;
+import com.example.Backend.Models.Like;
+import com.example.Backend.Models.Postovi;
 import com.example.Backend.Models.User;
 import com.example.Backend.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,9 +24,21 @@ public class UserServices {
         return userRepository.save(user);
     }
 
-    public Optional<User> findUser(String ime, String Passwod) {
-        Optional<User> user = userRepository.findByImeAndPassword(ime, Passwod);
+    public Optional<User> findUser(UserLoginDTO userLoginDTO) {
+        Optional<User> user = userRepository.findByImeAndPassword(userLoginDTO.getEmail(), userLoginDTO.getPassword());
         return user;
     }
 
+    public UserDTO find(Long id){
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User nije pronadjen"));
+        List<Long> likes = new ArrayList<>();
+        for(Like like : user.getLajkovaneObjave()){
+            likes.add(like.getUser().getId());
+        }
+        List<Long> posts =new ArrayList<>();
+        for (Postovi postovi : user.getPosts()){
+            posts.add(user.getId());
+        }
+        return new UserDTO(user.getIme(), user.getPrezime(), user.getEmail(), posts, likes);
+    }
 }
